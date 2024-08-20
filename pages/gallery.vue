237474d9686d1f9db2333f3309801c6d7d5c4460
DannielLima <karlosdanniellima@gmail.com>
2024-08-20 09:52:26 -0300

    adicionando home e galeria

---
+++ pages/gallery.vue
@@ -0,0 +1,33 @@
+<template>
+    <section class="py-10 bg-gray-50">
+      <h2 class="text-3xl font-bold text-gray-800 mb-8 text-center">Image Gallery</h2>
+      <div class="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4">
+        <div
+          v-for="(image, index) in images"
+          :key="index"
+          class="relative group overflow-hidden rounded-lg shadow-lg transform transition-transform duration-300 hover:scale-105"
+        >
+          <img
+            :src="image"
+            alt="Gallery Image"
+            class="w-full h-full object-cover"
+          />
+          <div class="absolute inset-0 bg-black bg-opacity-50 opacity-0 group-hover:opacity-100 transition-opacity duration-300 flex items-center justify-center">
+            <span class="text-white text-lg font-bold">View Image</span>
+          </div>
+        </div>
+      </div>
+    </section>
+  </template>
+  
+  <script setup>
+  const images = [
+    '/images/foto1.avif',
+    '/images/foto2.avif',
+    '/images/foto3.avif',
+    '/images/foto4.avif',
+    '/images/foto5.avif',
+    '/images/foto6.avif',
+    ];
+  </script>
+  
---
+++ pages/index.vue
@@ -0,0 +1,42 @@
+<template>
+    <section class="flex items-center justify-center h-screen bg-gradient-to-r from-blue-500 to-purple-500">
+      <div class="text-center text-white p-8 rounded-lg bg-opacity-90 backdrop-filter backdrop-blur-lg">
+        <h1 class="text-5xl font-extrabold mb-6 animate-fade-in-down">Welcome to VSQO</h1>
+        <p class="text-lg mb-8 animate-fade-in-up">Capture and share your best shots.</p>
+        <NuxtLink to="/gallery" class="text-lg font-semibold bg-white text-blue-600 px-6 py-3 rounded-full hover:bg-blue-100 transition-colors">Explore Gallery</NuxtLink>
+      </div>
+    </section>
+  </template>
+  
+  <style>
+  @keyframes fade-in-down {
+    0% {
+      opacity: 0;
+      transform: translateY(-20px);
+    }
+    100% {
+      opacity: 1;
+      transform: translateY(0);
+    }
+  }
+  
+  @keyframes fade-in-up {
+    0% {
+      opacity: 0;
+      transform: translateY(20px);
+    }
+    100% {
+      opacity: 1;
+      transform: translateY(0);
+    }
+  }
+  
+  .animate-fade-in-down {
+    animation: fade-in-down 1s ease-out forwards;
+  }
+  
+  .animate-fade-in-up {
+    animation: fade-in-up 1s ease-out forwards;
+  }
+  </style>
+  
