d7a42699611013f04ca2cda34a5b25e0fb4e7d00
DannielLima <karlosdanniellima@gmail.com>
2024-08-20 09:38:34 -0300

    configurando tailwind

--- app.vue
+++ app.vue
@@ -1,6 +1,20 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <header class="bg-gray-800 p-4">
+      <nav class="container mx-auto flex justify-between text-white">
+        <h1 class="text-2xl font-bold">VSQO</h1>
+        <ul class="flex space-x-4">
+          <li><NuxtLink to="/">Home</NuxtLink></li>
+          <li><NuxtLink to="/gallery">Gallery</NuxtLink></li>
+          <li><NuxtLink to="/about">About</NuxtLink></li>
+        </ul>
+      </nav>
+    </header>
+    <main class="container mx-auto py-8">
+      <NuxtPage />
+    </main>
+    <footer class="bg-gray-800 text-center text-white py-4">
+      © 2024 VSQO. All rights reserved.
+    </footer>
   </div>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
+module.exports = {
+  content: [
+    './app.vue',
+    './components/**/*.{vue,js}',
+    './layouts/**/*.{vue,js}',
+    './pages/**/*.{vue,js}',
+    './plugins/**/*.{js,ts}',
+    './nuxt.config.{js,ts}',
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
